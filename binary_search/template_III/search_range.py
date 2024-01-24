159d6963a79b8657599b65350b74fc2c81c558cf
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-25 00:53:06 +0400

    accepted 1st iteration search_range

--- binary_search/template_III/search_range.py
+++ binary_search/template_III/search_range.py
@@ -4,14 +4,18 @@ def searchLeft(nums, target, l, r):
     
     while l + 1 < r:
         m = (l + r) // 2
-        if nums[m-1]:
-            if nums[m] == target and nums[m-1] != target:
-                return m
+        if nums[m-1] != -1:
+            if nums[m] == target:
+                r = m
+                if nums[m-1] != target:
+                    return m
             elif nums[m] < target:
                 l = m
             else:
                 r = m
-    print(l, r)
+    if nums[l] == target: return l
+    if nums[r] == target: return r
+
     return -1
         
 def searchRight(nums, target, l, r):
@@ -21,18 +25,23 @@ def searchRight(nums, target, l, r):
     while l + 1 < r:
         m = (l + r) // 2
         if nums[m+1] != len(nums)-1:
-            if nums[m] == target and nums[m+1] != target:
-                return m
+            if nums[m] == target:
+                l = m
+                if nums[m+1] != target:
+                    return m
             elif nums[m] < target:
                 l = m
-            else:
+            elif nums[m] > target:
                 r = m
-    print(l, r)
+    if nums[l] == target: return l
+    if nums[r] == target: return r
     return -1
 
 def searchRange(nums, target):
     if len(nums) == 0:
         return (-1, -1)
+    # if len(nums) == 2:
+    #     return (0, 1)
     
     strt_idx, end_idx = (-1, -1)
     left, right = 0, len(nums)-1
@@ -42,15 +51,17 @@ def searchRange(nums, target):
         if nums[mid] == target:
             strt_idx = searchLeft(nums, target, left, mid)
             end_idx = searchRight(nums, target, mid, right)
-            print("idxs", end=" ")
-            print(strt_idx, end_idx)
-            break
+            return (strt_idx, end_idx)
         elif nums[mid] < target:
             left = mid
         else:
             right = mid
     
-    print("left", left)
-    print("right", right)
+    if nums[left] == target and nums[right] == target:
+        return (left, right)
+    if nums[left] == target: return (left, left)
+    if nums[right] == target: return (right, right)
+    return (-1, -1)
 
-searchRange([3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7], 6)
+ans = searchRange([0, 1, 1, 2, 3, 3, 5], 2)
+print(ans)
