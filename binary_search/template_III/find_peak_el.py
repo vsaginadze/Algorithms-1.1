0ab0b85ae6a4a44b40255161d4b847e68c6c6c50
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-25 21:45:23 +0400

    accepted version find_peak_el

---
+++ binary_search/template_III/find_peak_el.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def findPeakElement(self, arr: List[int]) -> int:
+        lo, hi = 0, len(arr)-1
+        while lo < hi:
+            mid = (lo + hi) // 2
+                
+            if arr[mid] > arr[mid+1] and arr[mid] > arr[mid-1]:
+                return mid
+            if arr[mid+1] > arr[mid]:
+                lo = mid+1
+            elif arr[mid-1] > arr[mid]:
+                hi = mid
+            
+        return lo
