360e0c08e3c15f4857d73b64cd7f6bd9c9cf08a8
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-26 22:18:48 +0400

    "solved" pow

--- binary_search/conclusion/pow.py
+++ binary_search/conclusion/pow.py
@@ -1,3 +1,3 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n # How am I supposed 
+        return x ** n # How am I supposed to solve this fckng problem with BSSSSSSSSS you dog shit? you chickenshit, bullshit, horseshit, shit?
