48a4eedca60d46e3166f8de5aaafeb24cff6f96a
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-29 22:52:54 +0400

    accepted version of linked_list_cycle_II

--- linked_list/two-pointer_technique/linked_list_cycle_II.py
+++ linked_list/two-pointer_technique/linked_list_cycle_II.py
@@ -1,36 +1,25 @@
 from typing import Optional
-
 class ListNode:
-    def __init__(self, x, next=None):
-        self.val = x
+    def __init__(self, val, next=None):
+        self.val = val
         self.next = next
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head and head.next:
-            slow = head.next
-            fast = head.next.next
-            
-            while slow is not fast:
+            slow = fast = head
+
+            while fast is not None and fast.next is not None:
+                slow = slow.next
                 fast = fast.next.next
-                slow = slow.next
+                
+                if slow == fast:
+                    slow = head
 
-            slow = head
-            
-            while slow is not fast:
-                fast = fast.next
-                slow = slow.next
-            
-            print(slow.val)
+                    while slow != fast:
+                        slow = slow.next
+                        fast = fast.next
 
-
-tail = ListNode(4)
-n3 = ListNode(0, tail)
-n2 = ListNode(2, n3)
-n1 = ListNode(1, n2)
-head = ListNode(3, n1)
-
-tail.next = n2
-
-sol = Solution()
-sol.detectCycle(head)
+                    return slow
+        
+        return None
