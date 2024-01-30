f18c5b95e21a81a28ce0eec64c64f36dc26ded27
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-30 23:18:36 +0400

    accepted version of remove_nth_from_end

--- linked_list/two-pointer_technique/remove_nth_from_end.py
+++ linked_list/two-pointer_technique/remove_nth_from_end.py
@@ -4,16 +4,6 @@ class ListNode:
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
-    
-    def __len__(self, headL):
-        node = headL
-        size = 0
-
-        while node:
-            node = node.next
-            size += 1
-
-        return size
 
 class Solution:
     def __len__(self, headL):
@@ -25,38 +15,44 @@ class Solution:
             size += 1
 
         return size
-    
-    
 
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        a = head
-        print(a.val)
+        a = ListNode(None, head)
         length = self.__len__(a)
-        removeAt = length - n
 
+        if n == 1 == length: return None 
+        
+        removeAt = length - n
         if removeAt > 0:
-            cnt = 0
+            cnt = 1
             node = a
             while node:
                 if cnt == removeAt:
                     node.next = node.next.next
                     break
                 node = node.next
+                cnt += 1
         
-        return a
+        return a.next
 
 def display(head):
+        if head is None: 
+            print("None")
+            return
+        
         current = head
-        while current is not None:
+        while current.next:
             print(current.val, end=" -> ")
             current = current.next
-        print("None")
+        print(current.val)
 
-lst = ListNode(5, ListNode(6, ListNode(7, ListNode(8))))
+# lst = ListNode(5, ListNode(6, ListNode(7, ListNode(8))))
+lst = ListNode(1, ListNode(2))
+n = 2
 display(lst)
 
 sol = Solution()
-head = sol.removeNthFromEnd(lst, 2)
+head = sol.removeNthFromEnd(lst, n)
 
 display(head)
 
