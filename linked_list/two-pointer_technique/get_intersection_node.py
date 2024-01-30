6521bb9f335bfebfa04a1445be541daa963f03ff
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-30 20:59:46 +0400

    starting get_intersection_node

---
+++ linked_list/two-pointer_technique/get_intersection_node.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        pass
+    
