a3ec134512b7f59890ccd36abb2651e7695595d2
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-30 22:47:30 +0400

    starting remove_nth_from_end

---
+++ linked_list/two-pointer_technique/remove_nth_from_end.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        pass
+    
