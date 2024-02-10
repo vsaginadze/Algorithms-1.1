3050456e262f5af7f5d4ac0f89fb0a14c3e22d98
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-11 00:04:39 +0400

    second attempt on remove_elements

--- linked_list/classic_problems/remove_elements.py
+++ linked_list/classic_problems/remove_elements.py
@@ -3,20 +3,24 @@ from linked_list import LinkedList, ListNode
 
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        node = ListNode(None, head)
-        
-        while node.next:
-            if node.next.val == val:
-                node.next = node.next.next
-            node = node.next
-        
-        return head
+        if head:
+            node = head = ListNode(None, head)
+            
+            while node.next:
+                while node.next.val == val:
+                    if node.next.next is None: return
+                    node.next = node.next.next
+                node = node.next
 
-arr = [1,2,2,2,3,5]
+            return head.next
+        
+        return None
+
+arr = [2,2,2,2,2]
 
 lst = LinkedList(arr)
 lst.array_to_linked_list()
-lst.display()
+lst.head.display()
 
 sol = Solution()
 head = sol.removeElements(lst.head, 2)
