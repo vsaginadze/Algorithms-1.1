b00a28de705d9d2bbb572480f0779ac657ae2249
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-10 23:32:27 +0400

    accepted version of reverse_linked_list

--- linked_list/classic_problems/reverse_linked_list.py
+++ linked_list/classic_problems/reverse_linked_list.py
@@ -1,10 +1,30 @@
 from typing import Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+from node import ListNode
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        if head:
+            first_node = head
+
+            while first_node.next:
+                next_node = first_node.next
+                first_node.next = next_node.next
+                next_node.next = head
+                head = next_node
+
+            return head
+        
+        return None
+
+
+# n3 = ListNode(7, None)
+# n2 = ListNode(15, n3)
+n1 = ListNode(6, None)
+head = ListNode(23, None)
+
+head.display()
+
+sol = Solution()
+
+head = sol.reverseList(head)
+head.display()
