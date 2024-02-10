4bf3b1a64ffaa49dad4b4419b5fe2bcf7630242b
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-11 01:06:01 +0400

    starting odd_even_list

---
+++ linked_list/classic_problems/odd_even_list.py
@@ -0,0 +1,6 @@
+from typing import Optional
+from linked_list import LinkedList, ListNode
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
