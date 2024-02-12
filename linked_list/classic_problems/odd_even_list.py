3ea9f0cefa367d925e21fa2151eec7619b37d849
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-13 01:08:47 +0400

    deleted tests

--- linked_list/classic_problems/odd_even_list.py
+++ linked_list/classic_problems/odd_even_list.py
@@ -20,18 +20,3 @@ class Solution:
                 e = e.next
 
         return head
-
-
-
-arr = [2,1,3,5,6,4,7]
-arr1 = [1,2,3,4,5, 6]
-lst = LinkedList(arr1)
-lst.array_to_linked_list()
-head = lst.head
-
-head.display()
-
-sol = Solution()
-head = sol.oddEvenList(head)
-
-head.display()
