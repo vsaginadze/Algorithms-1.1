0560354299f242e101139a3d104a771884ac1884
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-12 22:56:23 +0400

    accepted version

--- linked_list/classic_problems/odd_even_list.py
+++ linked_list/classic_problems/odd_even_list.py
@@ -3,12 +3,28 @@ from linked_list import LinkedList, ListNode
 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        if not head: return None
+        
+        if head.next:
+            e = eHead = head.next
+            oTail = head
+
+            while e:
+                o = e.next
+                if o:
+                    e.next = o.next
+                    o.next = eHead
+                    oTail.next = o
+                    oTail = o
+
+                e = e.next
+
+        return head
 
 
 
 arr = [2,1,3,5,6,4,7]
-arr1 = [1,2,3,4]
+arr1 = [1,2,3,4,5, 6]
 lst = LinkedList(arr1)
 lst.array_to_linked_list()
 head = lst.head
