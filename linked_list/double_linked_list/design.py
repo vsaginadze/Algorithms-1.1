e21fbd9e08972798192e4a9ba621c97b92cb64ad
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-15 23:19:11 +0400

    added addAtHead method to double_linked_list

--- linked_list/double_linked_list/design.py
+++ linked_list/double_linked_list/design.py
@@ -4,6 +4,26 @@ class DoublyListNode:
         self.next = next
         self.prev = None
 
+class DoublyLinkedList:
+    def __init__(self, head):
+        self.head = head
+    
+    def display(self):
+        if self.head:
+            print(f'None <- ({self.head.val}) -> ', end="")
+
+            cur_node = self.head.next
+
+            while cur_node:
+                print(f"<- ({cur_node.val}) -> ", end="")
+                cur_node = cur_node.next
+            print("None")
+    
+    def addAtHead(self, val):
+        node = DoublyListNode(val, self.head)
+        self.head.prev = node
+        self.head = node
+
 n3 = DoublyListNode(3)
 n2 = DoublyListNode(2, n3)
 n1 = DoublyListNode(1, n2)
@@ -13,17 +33,8 @@ n3.prev = n2
 n2.prev = n1
 n1.prev = head
 
+    
+doubly_list = DoublyLinkedList(head)
 
-
-def display(head):
-    if head:
-        print(f'None <- {head.val} -> ', end="")
-
-        cur_node = head.next
-
-        while cur_node:
-            print(f"<- {cur_node.val} -> ", end="")
-            cur_node = cur_node.next
-        print("None")
-        
-display(head)
+doubly_list.addAtHead(-1)
+doubly_list.display()
