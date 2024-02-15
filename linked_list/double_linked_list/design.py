7f152ec7df9548966928d6e4365631a16908329f
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-15 22:58:54 +0400

    added display function

--- linked_list/double_linked_list/design.py
+++ linked_list/double_linked_list/design.py
@@ -1,6 +1,29 @@
 class DoublyListNode:
-    def __init__(self, val, next=None, prev=None):
+    def __init__(self, val, next=None):
         self.val = val
         self.next = next
-        self.prev = prev
+        self.prev = None
 
+n3 = DoublyListNode(3)
+n2 = DoublyListNode(2, n3)
+n1 = DoublyListNode(1, n2)
+head = DoublyListNode(0, n1)
+
+n3.prev = n2
+n2.prev = n1
+n1.prev = head
+
+
+
+def display(head):
+    if head:
+        print(f'None <- {head.val} -> ', end="")
+
+        cur_node = head.next
+
+        while cur_node:
+            print(f"<- {cur_node.val} -> ", end="")
+            cur_node = cur_node.next
+        print("None")
+        
+display(head)
