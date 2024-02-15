95b75ba355669191cfd30b256a0f09ea5510108d
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-16 00:22:44 +0400

    added deleteAtIndex method to design

--- linked_list/double_linked_list/design.py
+++ linked_list/double_linked_list/design.py
@@ -4,9 +4,9 @@ class DoublyListNode:
         self.next = next
         self.prev = None
 
-class DoublyLinkedList:
-    def __init__(self, head):
-        self.head = head
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
     
     def display(self) -> None:
         if self.head:
@@ -21,8 +21,11 @@ class DoublyLinkedList:
     
     def addAtHead(self, val) -> None:
         node = DoublyListNode(val, self.head)
-        self.head.prev = node
-        self.head = node
+        if self.head:
+            self.head.prev = node
+            self.head = node
+        else:
+            self.head = node
     
     def addAtTail(self, val) -> None:
         node = DoublyListNode(val)
@@ -39,7 +42,6 @@ class DoublyLinkedList:
     def get(self, index: int) -> int:
         if not index >= 0: return -1
         idx, current_node = 0, self.head
-        
         while current_node:
             if idx == index:
                 return current_node.val
@@ -51,7 +53,7 @@ class DoublyLinkedList:
     def addAtIndex(self, index: int, val: int) -> None:
         if not index >= 0: return
 
-        idx, cur_node = 1, self.head.next
+        idx, cur_node = 0, self.head
         node = DoublyListNode(val)
         if index:
             while cur_node:
@@ -64,23 +66,17 @@ class DoublyLinkedList:
                 cur_node = cur_node.next
         else:
             self.addAtHead(val)
-
-
-
-
-n3 = DoublyListNode(3)
-n2 = DoublyListNode(2, n3)
-n1 = DoublyListNode(1, n2)
-head = DoublyListNode(0, n1)
-
-n3.prev = n2
-n2.prev = n1
-n1.prev = head
-
     
-doubly_list = DoublyLinkedList(head)
+    def deleteAtIndex(self, index: int) -> None:
+        if not index >= 0: return
+        idx, current_node = 1, self.head
+        if index:
+            while current_node.next:
+                current_node = current_node.next
+                if idx == index:
+                    current_node.prev.next = current_node.next
+                    return
+                idx += 1
+        else:
+            self.head = self.head.next
 
-doubly_list.addAtHead(-1)
-doubly_list.addAtTail(4)
-doubly_list.addAtIndex(-1, 99)
-doubly_list.display()
