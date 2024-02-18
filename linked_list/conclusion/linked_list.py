6e21368263ae3b36f324783417e0335eaeddbd80
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-18 18:31:38 +0400

    accepted version of merge_two_lists

---
+++ linked_list/conclusion/linked_list.py
@@ -0,0 +1,42 @@
+class ListNode:
+        def __init__(self, val, next=None):
+            self.val = val
+            self.next = next
+            
+        def display(self):
+            current_node = self
+
+            while current_node.next:
+                print(current_node.val, end=" -> ")
+                current_node = current_node.next
+            
+            print(current_node.val)
+
+class LinkedList:
+    def __init__(self, arr=[]):
+        self.head = None
+        self.arr = arr
+
+        self.array_to_linked_list()
+
+    def array_to_linked_list(self):
+        if not self.arr:
+            return None
+        
+        self.head = ListNode(self.arr[0])
+        current_node = self.head
+
+        for value in self.arr[1:]:
+            current_node.next = ListNode(value)
+            current_node = current_node.next
+        
+        
+
+    def display(self):
+        current_node = self.head
+
+        while current_node.next:
+            print(current_node.val, end=" -> ")
+            current_node = current_node.next
+        
+        print(current_node.val)
--- linked_list/conclusion/merge_two_lists.py
+++ linked_list/conclusion/merge_two_lists.py
@@ -1,9 +1,39 @@
 from typing import Optional
-from linked_list import ListNode
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+from linked_list import ListNode, LinkedList
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        sorted_list = ListNode(None, None)
+        node = sorted_list
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            
+            node = node.next
         
+        if list1:
+            node.next = list1
+        elif list2:
+            node.next = list2
+        
+        return sorted_list.next
+        
+
+
+list1 = [0,2,5]
+list2 = [1,3,4]
+
+lst1 = LinkedList(list1)
+lst2 = LinkedList(list2)
+
+lst1.display()
+lst2.display()
+
+sol = Solution()
+sorted_lst = sol.mergeTwoLists(lst1.head, lst2.head)
+sorted_lst.display()
