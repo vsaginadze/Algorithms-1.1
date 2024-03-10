b1d73d67e9cf8558b00a23b2ce04817dc96b48c3
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-11 01:39:08 +0400

    accepted version of sort_colors

--- sorting/comparison_based_sorts/sort_colors.py
+++ sorting/comparison_based_sorts/sort_colors.py
@@ -1,5 +1,16 @@
+from typing import List
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        for i in range(len(nums)):
+            min_idx = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[min_idx]:
+                    min_idx = j
+            
+            nums[min_idx], nums[i] = nums[i], nums[min_idx]
+        
+
+nums = [2,0,1]
+sol = Solution()
+sol.sortColors(nums)
