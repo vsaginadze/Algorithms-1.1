e559f83dcc4fd9286f6ff613ca672ed2a7c6512a
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-11 04:48:07 +0400

    accepted version of find_kth_largest

--- find_kth_largest.py
+++ find_kth_largest.py
@@ -2,18 +2,30 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, lst: List[int], k: int) -> int:
-        if len(lst) == 1 and k == 1: return lst[0]
-        for i in range(k):
-            max_index = i
-            for j in range(i + 1, len(lst)):
-                # Update minimum index
-                if lst[j] > lst[max_index]:
-                    max_index = j
-
-            # Swap current index with minimum element in rest of list
-            lst[max_index], lst[i] = lst[i], lst[max_index]
+        def max_heapify(heap_size, index):
+            left, right = 2 * index + 1, 2 * index + 2
+            largest = index
+            
+            if left < heap_size and lst[left] > lst[largest]:
+                largest = left
+            if right < heap_size and lst[right] > lst[largest]:
+                largest = right
+            if largest != index:
+                lst[largest], lst[index] = lst[index], lst[largest]
+                max_heapify(heap_size, largest)
         
-        return lst[k]
+    
+        # max heapify the list
+        for i in range(len(lst) // 2 - 1, -1, -1):
+            max_heapify(len(lst), i)
+        
+        for i in range(len(lst)-1,len(lst)-k-1,-1):
+            # swap largest(first) element with the last
+            lst[i], lst[0] = lst[0], lst[i]
+            # notice that we reduce the lst size by one on each iteration
+            max_heapify(i, 0)
+
+        return lst[len(lst)-k]
 
 sol = Solution()
 arr = [3,2,1,5,6,4] 
