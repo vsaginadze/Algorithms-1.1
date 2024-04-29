cad498958bc51ec9cf547dcea104fe4117683e7d
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-04-29 22:59:38 +0400

    finished smallest_trimmed_numbers I hate life

--- sorting/non-comparison_based_sorts/smallest_trimmed_numbers.py
+++ sorting/non-comparison_based_sorts/smallest_trimmed_numbers.py
@@ -1,46 +1,11 @@
 from typing import List
 
 class Solution:
-    def smallestTrimmedNumbers(self, lst: List[str], queries: List[List[int]]) -> List[int]:
-        results = []
+    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
+        result = []
         for pair in queries:
-            min, trim = pair
-
-            counts = [0] * 10
-
-            for el in lst:
-                digit = int(el[-trim])
-                counts[digit] += 1
-        
-            # starting_index = 0
-            # for i, count in enumerate(counts):
-            #     counts[i] = starting_index
-            #     starting_index += count
-            
-            
-            # sorted_lst = [0] * len(lst)
-            # for elem in lst:
-            #     digit = int(el[-trim])
-            #     sorted_lst[counts[digit]] = elem
-            #     counts[digit] += 1
-            
-
-
-lst = ["64333639502","65953866768","17845691654","87148775908","58954177897","70439926174","48059986638","47548857440","18418180516","06364956881","01866627626","36824890579","14672385151","71207752868"]
-queries = [[9,4],[6,1],[3,8],[12,9],[11,4],[4,9],[2,7],[10,3],[13,1],[13,1],[6,1],[5,10]]
-
-sol = Solution()
-sol.smallestTrimmedNumbers(lst, queries)
-
-'''
-nums = ["102","473","251","814"], queries = [[1,1],[2,3],[4,2],[1,2]]
-k_th, trim = pair -> [1,1]
-
-1. crop `trim` numbers from behind to every nums element
-arr -> ["2", "3", "1", "4"]
-
-2. find k_th number in arr
-
-3. get index of arr[k_th] in nums
-
-'''
+            k, trim = pair
+            arr = [(int(num[-trim:]), i) for i, num in enumerate(nums)]
+            print(arr)
+            result.append[arr[k-1][1]]
+        return result
